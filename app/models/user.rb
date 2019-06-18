37fc61c6e7cf51ca5df6a46cbd2d10aa745bd4cc
Ronny <polleronny@yahoo.com>
2019-06-18 00:03:59 +0000

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+
+    attr_accessible :name, :email, :address,:phone,:password,:password_confirm
+    has_secure_password
+    
+    before_save ( |user| user.email = email.downcase )
     before_save :create_remember_token
 
 
