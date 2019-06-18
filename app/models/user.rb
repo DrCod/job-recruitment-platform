1ff5b9f390157c503c9210d4d6ba36d1f976816c
Ronny <polleronny@yahoo.com>
2019-06-18 00:12:19 +0000

    Fixes syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
 
     attr_accessible :name, :email, :address,:phone,:password,:password_confirm
     has_secure_password
-    
-    before_save ( |user| user.email = email.downcase )
+    before save { |user| user.email = email.downcase }
     before_save :create_remember_token
 
 
