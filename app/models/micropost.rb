6e9fa9cdcdcba6599c4409b6ea175c642ed131e8
Ronny <polleronny@yahoo.com>
2019-06-24 18:25:49 +0000

    Edit sql query

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,9 +8,9 @@ class Micropost < ApplicationRecord
 
 
     def self.from_users_followed_by(user)
-        followed_users_id ="SELECT followed id FROM relationships
-        WHERE follower id = :user id"
-        where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",user_id: user.id)
+        followed_id ="SELECT followed_id FROM relationships
+        WHERE follower_id = :user_id"
+        where("user_id IN (#{followed_id}) OR user_id = :user_id",user_id: user.id)
     end
 
 end
